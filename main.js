3b241475e0fe2e6cdc7d829a6708e617495ba237
Rodney Fowlkes <Fowlkesrodney@gmail.com>
2016-06-20 16:24:46 -0400

    commit before changing the clock to reflect current time

--- main.js
+++ main.js
@@ -13,13 +13,14 @@ if (date_sec == 0) {minutes += 1;};
 var date_min = minutes % 60;
 now.setMinutes(date_min);
 
-if (date_min == 0) {hours += 1;};
+if (date_min == 0 && date_sec == 0) {hours += 1;};
 var date_hours = hours % 60;
-now.setHours(date_min);
+now.setHours(date_hours);
 
 
-document.querySelector(".clock").textContent = now;
+// document.querySelector(".clock").textContent = now;
 console.log(date_sec);
+console.log("minutes:" + date_min);
 document.querySelector(".hours").textContent = now.getHours();
 document.querySelector(".minutes").textContent = now.getMinutes();
 document.querySelector(".seconds").textContent = now.getSeconds();
